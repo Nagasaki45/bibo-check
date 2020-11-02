945294ded0c7b61f90c6abe041bd23d96d29b37f
Tom Gurion <nagasaki45@gmail.com>
2020-11-02 10:39:27 +0000

    Nicer output

--- bibo_check/__init__.py
+++ bibo_check/__init__.py
@@ -4,14 +4,14 @@ import bibo.internals
 import click
 
 
-def missing_files(data):
+def missing_files_gen(data):
     for entry in data:
         file_field = entry['fields'].get('file')
         if file_field and not os.path.exists(file_field):
             yield entry['key'], file_field
 
 
-def files_without_entries(data, files_dir):
+def files_with_no_entries_gen(data, files_dir):
     expected_files = {e['fields']['file'] for e in data if 'file' in e['fields']}
     for filename in os.listdir(files_dir):
         filepath = os.path.join(files_dir, filename)
@@ -23,12 +23,16 @@ def files_without_entries(data, files_dir):
 @click.pass_context
 def check(ctx):
     data = ctx.obj['data']
-    click.echo('Missing files')
-    for key, missing_file in missing_files(data):
-        click.echo('{}\t{}'.format(key, missing_file))
+    missing_files = list(missing_files_gen(data))
+    if missing_files:
+        click.secho('Missing files', fg='red')
+        for key, missing_file in missing_files:
+            click.echo('{}\t{}'.format(key, missing_file))
 
     files_dir = bibo.internals.destination_heuristic(data)
 
-    click.echo('Files with no entries')
-    for file_without_entry in files_without_entries(data, files_dir):
-        click.echo(file_without_entry)
+    files_with_no_entries = list(files_with_no_entries_gen(data, files_dir))
+    if files_with_no_entries:
+        click.secho('Files with no entries', fg='red')
+        for file_with_no_entry in files_with_no_entries:
+            click.echo(file_with_no_entry)
